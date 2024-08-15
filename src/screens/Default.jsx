4ca8e9f91f1bea9100bdebfca130a6ba857dd7b5
Diego Viscarra <diego_viscarra@hotmail.es>
2024-08-14 21:18:20 -0400

    Init prod

--- src/screens/Default.jsx
+++ src/screens/Default.jsx
@@ -1,7 +1,6 @@
-import Logo from '../components/Logo';
-import Description from '../components/Description';
-import Content from '../components/Content';
-import GitHub from '../components/GitHub';
+import Logo from './_components/Logo';
+import Description from './_components/Description';
+import OpenLink from '../components/OpenLink';
 
 const Default = () => {
     return (
@@ -9,8 +8,8 @@ const Default = () => {
             <header className="App-header">
                 <Logo />
                 <Description />
-                <Content />
-                <GitHub />
+                <OpenLink title="Learn React" url="https://reactjs.org" />
+                <OpenLink title="GitHub Repository" url="https://github.com/DiegoViscarra/curso-2024-desarrollo-frontend-react" />
             </header>
         </>
     );
