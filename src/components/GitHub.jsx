3bbdc482d5b6b4c5c6f5c6ef447662ddcc1062ad
Diego Viscarra <diego_viscarra@hotmail.es>
2024-08-14 21:01:43 -0400

    Init components

---
+++ src/components/GitHub.jsx
@@ -0,0 +1,16 @@
+const GitHub = () => {
+    return (
+        <div style={{marginTop: "12px"}}>
+            <a
+                className="App-link"
+                href="https://github.com/DiegoViscarra/curso-2024-desarrollo-frontend-react"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                GitHub Repository
+            </a>
+        </div>
+    );
+};
+
+export default GitHub;
