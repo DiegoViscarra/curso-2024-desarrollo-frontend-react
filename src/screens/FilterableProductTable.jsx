802f2ab57ee4c2a322aad8dfeeb2ec314b433f7a
Diego Viscarra <diego_viscarra@hotmail.es>
2024-08-16 10:24:34 -0400

    Added Homework

---
+++ src/screens/FilterableProductTable.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import SearchBar from "./_components/SearchBar";
+import ProductTable from "./_components/ProductTable";
+
+const FilterableProductTable = ({ products }) => {
+    const [filterText, setFilterText] = useState('');
+    const [inStockOnly, setInStockOnly] = useState(false);
+
+    return (
+        <div>
+            <SearchBar 
+                filterText={filterText} 
+                inStockOnly={inStockOnly} 
+                onFilterTextChange={setFilterText} 
+                onInStockOnlyChange={setInStockOnly} />
+            <ProductTable 
+                products={products} 
+                filterText={filterText}
+                inStockOnly={inStockOnly} />
+        </div>
+    );
+};
+
+export default FilterableProductTable;
